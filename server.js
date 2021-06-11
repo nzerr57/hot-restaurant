68bc55b21a0403c594c921202662a7db666fe883
NZerr <nathan.zerr@gmail.com>
2021-06-11 13:16:35 -0400

    initial commit

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.send('Welcome to Hot Restaurant');
+});
+
+app.listen(PORT, () => {
+    console.log(`Listening at http://localhost:${PORT}`);
+});
